1a4cce7b0856c7f8ccfa1c1e8be2528ca14ca907
AmmarFarid10 <ammar.ahmad.farid@gmail.com>
2024-05-31 22:07:18 +0500

    new api view added

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -33,3 +33,35 @@ class BlogPostList(APIView):
             
         serializer = BlogPostSerializer(blog_posts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    def put(self, request, pk, format=None):
+        print(pk)
+        blog_post = self.get_object(pk)
+        serializer = BlogPostSerializer(blog_post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class BlogPostDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return BlogPost.objects.get(pk=pk)
+        except BlogPost.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        blog_post = self.get_object(pk)
+        serializer = BlogPostSerializer(blog_post)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        blog_post = self.get_object(pk)
+        serializer = BlogPostSerializer(blog_post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        blog_post = self.get_object(pk)
+        blog_post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
