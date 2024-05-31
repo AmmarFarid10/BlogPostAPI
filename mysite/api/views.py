77e37f95e86dad63414c2408ea6877176fca78ad
AmmarFarid10 <ammar.ahmad.farid@gmail.com>
2024-05-31 19:19:44 +0500

    Customized Search view using GET

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .models import BlogPost
 from .serializers import BlogPostSerializer
 from django.shortcuts import render
+from rest_framework.views import APIView
 
 class BlogPostListCreate(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
@@ -15,3 +16,20 @@ class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
     lookuo_field ="pk"
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+class BlogPostList(APIView):
+    def get(self, request, format=None):
+        title = request.query_params.get("title", "")
+        logger.debug(f"Title parameter: {title}")
+        
+        if title:
+            blog_posts = BlogPost.objects.filter(title__icontains=title)
+        else:
+            blog_posts = BlogPost.objects.all()
+            
+        serializer = BlogPostSerializer(blog_posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
